0c66932e6b5a483db5ceba8b3ff010f4afe39ca4
imonweb <contact@imonweb.co.uk>
2022-02-08 23:39:28 +0000

    css

---
+++ app.js
@@ -0,0 +1,13 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listener
+todoButton.addEventListener('click', addTodo);
+
+// Function
+function addTodo(event){
+  event.preventDefault();
+  
+}
