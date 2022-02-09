7ea2e8f2b074af268a6913e887bee78972c61ecb
imonweb <contact@imonweb.co.uk>
2022-02-09 23:24:32 +0000

    css

--- app.js
+++ app.js
@@ -9,5 +9,26 @@ todoButton.addEventListener('click', addTodo);
 // Function
 function addTodo(event){
   event.preventDefault();
-  
+  //Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  //Check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerText - `<i class="fas fa-check"></i>`;
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  //Check trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerText - `<i class="fas fa-trash"></i>`;
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+
+  //Append to List
+  todoList.appendChild(todoDiv);
 }
+
